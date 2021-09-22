29326b9402f7027f9c861f0d358e086e34e3432e
saygnssmn <66674672+saygnssmn@users.noreply.github.com>
2021-09-22 14:33:28 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace TCP_IP_SerCli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbtnServer = new System.Windows.Forms.RadioButton();
+            this.rbtnClient = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lbIP = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.bxSend = new System.Windows.Forms.GroupBox();
+            this.btnClear1 = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.bxConsole = new System.Windows.Forms.GroupBox();
+            this.btnClear2 = new System.Windows.Forms.Button();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.bxSend.SuspendLayout();
+            this.bxConsole.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbtnServer
+            // 
+            this.rbtnServer.AutoSize = true;
+            this.rbtnServer.Checked = true;
+            this.rbtnServer.Location = new System.Drawing.Point(283, 22);
+            this.rbtnServer.Name = "rbtnServer";
+            this.rbtnServer.Size = new System.Drawing.Size(56, 17);
+            this.rbtnServer.TabIndex = 2;
+            this.rbtnServer.TabStop = true;
+            this.rbtnServer.Text = "Server";
+            this.rbtnServer.UseVisualStyleBackColor = true;
+            // 
+            // rbtnClient
+            // 
+            this.rbtnClient.AutoSize = true;
+            this.rbtnClient.Location = new System.Drawing.Point(283, 62);
+            this.rbtnClient.Name = "rbtnClient";
+            this.rbtnClient.Size = new System.Drawing.Size(51, 17);
+            this.rbtnClient.TabIndex = 3;
+            this.rbtnClient.Text = "Client";
+            this.rbtnClient.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(383, 20);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 59);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(61, 20);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(164, 20);
+            this.txtIP.TabIndex = 5;
+            this.txtIP.Text = "169.254.209.135";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(61, 60);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(81, 20);
+            this.txtPort.TabIndex = 6;
+            this.txtPort.Text = "5000";
+            // 
+            // lbIP
+            // 
+            this.lbIP.AutoSize = true;
+            this.lbIP.Location = new System.Drawing.Point(12, 24);
+            this.lbIP.Name = "lbIP";
+            this.lbIP.Size = new System.Drawing.Size(17, 13);
+            this.lbIP.TabIndex = 7;
+            this.lbIP.Text = "IP";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(12, 64);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 8;
+            this.lblPort.Text = "Port";
+            // 
+            // bxSend
+            // 
+            this.bxSend.Controls.Add(this.btnClear1);
+            this.bxSend.Controls.Add(this.btnSend);
+            this.bxSend.Controls.Add(this.txtSend);
+            this.bxSend.Location = new System.Drawing.Point(3, 105);
+            this.bxSend.Name = "bxSend";
+            this.bxSend.Size = new System.Drawing.Size(487, 78);
+            this.bxSend.TabIndex = 9;
+            this.bxSend.TabStop = false;
+            this.bxSend.Text = "Send";
+            // 
+            // btnClear1
+            // 
+            this.btnClear1.Location = new System.Drawing.Point(346, 45);
+            this.btnClear1.Name = "btnClear1";
+            this.btnClear1.Size = new System.Drawing.Size(132, 23);
+            this.btnClear1.TabIndex = 3;
+            this.btnClear1.Text = "Clear";
+            this.btnClear1.UseVisualStyleBackColor = true;
+            this.btnClear1.Click += new System.EventHandler(this.btnClear1_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(9, 45);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(132, 24);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(9, 19);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSend.Size = new System.Drawing.Size(469, 20);
+            this.txtSend.TabIndex = 1;
+            this.txtSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSend_KeyDown);
+            // 
+            // bxConsole
+            // 
+            this.bxConsole.Controls.Add(this.btnClear2);
+            this.bxConsole.Controls.Add(this.txtConsole);
+            this.bxConsole.Location = new System.Drawing.Point(3, 204);
+            this.bxConsole.Name = "bxConsole";
+            this.bxConsole.Size = new System.Drawing.Size(487, 199);
+            this.bxConsole.TabIndex = 10;
+            this.bxConsole.TabStop = false;
+            this.bxConsole.Text = "Console";
+            // 
+            // btnClear2
+            // 
+            this.btnClear2.Location = new System.Drawing.Point(346, 168);
+            this.btnClear2.Name = "btnClear2";
+            this.btnClear2.Size = new System.Drawing.Size(132, 23);
+            this.btnClear2.TabIndex = 4;
+            this.btnClear2.Text = "Clear";
+            this.btnClear2.UseVisualStyleBackColor = true;
+            this.btnClear2.Click += new System.EventHandler(this.btnClear2_Click);
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(9, 19);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConsole.Size = new System.Drawing.Size(469, 143);
+            this.txtConsole.TabIndex = 1;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.DarkRed;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStop.Location = new System.Drawing.Point(383, 20);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(107, 59);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 409);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.bxConsole);
+            this.Controls.Add(this.bxSend);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lbIP);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rbtnClient);
+            this.Controls.Add(this.rbtnServer);
+            this.Name = "Form1";
+            this.Text = "TCP/IP SerCli";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.bxSend.ResumeLayout(false);
+            this.bxSend.PerformLayout();
+            this.bxConsole.ResumeLayout(false);
+            this.bxConsole.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbtnServer;
+        private System.Windows.Forms.RadioButton rbtnClient;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lbIP;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.GroupBox bxSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.GroupBox bxConsole;
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear1;
+        private System.Windows.Forms.Button btnClear2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_IP_SerCli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+        SimpleTcpServer server;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnStart.BringToFront();
+            btnStop.SendToBack();
+            server = new SimpleTcpServer();
+            client = new SimpleTcpClient();
+            server.Delimiter = 0x13;//enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+            client.DataReceived += Client_DataReceived;
+            server.ClientConnected += Server_ClientConnected;
+            server.DelimiterDataReceived += Server_DelimiterDataReceived;
+
+        }
+
+        
+
+        private void Server_DelimiterDataReceived(object sender, SimpleTCP.Message e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Server_ClientConnected(object sender, TcpClient e)
+        {
+            txtConsole.Invoke((MethodInvoker)delegate ()
+            {
+                txtConsole.Text += $"Client connected!{Environment.NewLine}";
+            });
+        }
+
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            string x = "";
+            txtConsole.Invoke((MethodInvoker)delegate ()
+            {
+                x = e.MessageString;
+                if (!string.IsNullOrWhiteSpace(x.ToString()) && rbtnClient.Checked)
+                {
+                    txtConsole.Text += "Server: " + x.ToString() + Environment.NewLine;
+                }
+            });
+        }
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            string a = "";
+            txtConsole.Invoke((MethodInvoker)delegate ()
+            {
+                
+                a = e.MessageString;
+                if(!string.IsNullOrWhiteSpace(a.ToString()) && rbtnServer.Checked)
+                {
+                    txtConsole.Text += "Client: " + a.ToString() + Environment.NewLine;
+                }
+            });
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            //btnStop.BringToFront();
+            if (rbtnServer.Checked)
+            {
+                
+                try
+                {
+                    
+                    System.Net.IPAddress ipAddress = System.Net.IPAddress.Parse(txtIP.Text);
+                    server.Start(ipAddress, Convert.ToInt32(txtPort.Text));
+                    txtConsole.Text += $"Server started...{Environment.NewLine}";
+                    btnStop.BringToFront();
+                    txtIP.Enabled = false;
+                    txtPort.Enabled = false;
+                    rbtnClient.Enabled = false;
+                }
+                catch (Exception)
+                {
+                    btnStop.SendToBack();
+                    txtConsole.Text += "Could not start the server!" + Environment.NewLine;
+                }
+            }
+            if (rbtnClient.Checked)
+            {
+                //btnStart.SendToBack();
+                 
+                try
+                {
+                    
+                    //Connect to server
+                    client.Connect(txtIP.Text, Convert.ToInt32(txtPort.Text));
+                    btnStop.BringToFront();
+                    txtConsole.Text += "Connected to server" + Environment.NewLine;
+                    txtIP.Enabled = false;
+                    txtPort.Enabled = false;
+                    rbtnServer.Enabled = false;
+                }
+                catch (Exception)
+                {
+                    
+                    txtConsole.Text += "Could not connect to server!" + Environment.NewLine;
+                    btnStart.Enabled = true;
+                }
+            }
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted && rbtnServer.Checked)
+            {
+                server.Stop();
+                txtConsole.Text += $"Server stopped...{Environment.NewLine}";
+                btnStart.BringToFront();
+                btnStop.SendToBack();
+                txtIP.Enabled = true;
+                txtPort.Enabled = true;
+                rbtnClient.Enabled = true;
+            }
+            if(rbtnClient.Checked)
+            {
+                client.Disconnect();
+                txtConsole.Text += "Disconnected" + Environment.NewLine;
+                btnStart.BringToFront();
+                btnStop.SendToBack();
+                txtIP.Enabled = true;
+                txtPort.Enabled = true;
+                rbtnServer.Enabled = true;
+            }
+        }
+
+        private void txtSend_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSend_Click(this, new EventArgs());
+            }
+            
+        }
+
+        private void btnClear1_Click(object sender, EventArgs e)
+        {
+            txtSend.Text = "";
+        }
+
+        private void btnClear2_Click(object sender, EventArgs e)
+        {
+            txtConsole.Text = "";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (rbtnServer.Checked && !string.IsNullOrEmpty(txtSend.Text))
+            {
+                server.Broadcast(txtSend.Text);
+                txtConsole.Text += "Me: " + txtSend.Text + Environment.NewLine;
+            }
+            if (!string.IsNullOrEmpty(txtSend.Text)&&rbtnClient.Checked)
+            {
+                client.Write(txtSend.Text);
+                txtConsole.Text += "Me: " + txtSend.Text + Environment.NewLine;
+            }
+        }
+    }
+}
